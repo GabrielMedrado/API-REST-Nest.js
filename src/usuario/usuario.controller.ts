7db5d99cfd29d8c9ceb23b5f91258f24ef141626
Gabriel Medrado <medradoogabriel@gmail.com>
2024-02-29 17:37:37 -0300

    first commit

---
+++ src/usuario/usuario.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsuarioRepository } from './usuario.repository';
+
+@Controller('/usuarios')
+export class UsuarioController {
+  constructor(private usuarioRepository: UsuarioRepository) {}
+
+  @Post()
+  async criaUsuario(@Body() dadosDoUsuario) {
+    this.usuarioRepository.salvar(dadosDoUsuario);
+    return dadosDoUsuario;
+  }
+
+  @Get()
+  async listaUsuarios() {
+    return this.usuarioRepository.listar();
+  }
+}
